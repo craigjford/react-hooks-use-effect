ec3c23b585d44c2c0668530efc5269a74a27e6d4
Craig Ford <craig_ford@icloud.com>
2022-05-20 20:52:38 -0400

    Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
 import React, { useState, useEffect } from "react";
+import DogPics from "./DogPics";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState("");
+  
   useEffect(() => {
-    console.log("useEffect called");
-  });
+    document.title = text;
+  }, [text]);
 
-  console.log("Component rendering");
+  useEffect(() => {
+    setTimeout(() => setCount(0), 5000);
+  }, []);
 
-  return <button>Click Me</button>;
+  console.log("App Component rendering");
+
+  return (
+    <div>
+      <button onClick={() => setCount((count) => count + 1)}>
+          I have been Clicked {count} times
+      </button>
+      <input
+        type="text"
+        placeholder="Type away..."
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
+      <hr></hr>
+      <DogPics />
+    </div>
+  )
 }
 
 export default App;
--- src/DogPics.js
+++ src/DogPics.js
@@ -6,21 +6,21 @@ function DogPics() {
   const [images, setImages] = useState([]);
 
   useEffect(() => {
-    console.log("useEffect");
+    console.log("dogpics useEffect called");
     fetch("https://dog.ceo/api/breeds/image/random/3")
       .then((r) => r.json())
       .then((data) => {
         console.log("setState");
         setImages(data.message);
       });
-  });
+  }, []);
 
-  console.log("render");
+  console.log("render DogPics");
 
   return (
     <div>
       {images.map((image) => (
-        <img src={image} key={image} />
+        <img src={image} key={image} alt="Some other text" />
       ))}
     </div>
   );
